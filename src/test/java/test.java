b9e6a51ac3645bd5f6d29132a68d5d3c8b00cd26
MashaOsipova <mashaosi395@mail.ru>
2022-11-29 16:36:27 +0300

    commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-public class test1 {
+public class test {
     @Test
     void shouldPostIt() {
         given()
