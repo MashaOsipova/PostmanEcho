dc1a80df552d1ef4eed27e29ec58e4b3f31caef6
MashaOsipova <mashaosi395@mail.ru>
2022-11-29 16:34:40 +0300

    Initial commit

---
+++ src/test/java/test.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class test1 {
+    @Test
+    void shouldPostIt() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
